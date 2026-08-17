aa5dbd52a2c56885e1518a61cd4db055e4d0a244
zhiweizhang550 <zhiwei.zhang@gigadevice.com>
2026-08-17 15:26:22 +0800

    Merge pull request #61 from GD32-MCU-IOT/470i-eval
    [GD32][BSP] add GD32F470I-EVAL board support and F4xx peripheral driv…

--- bsp/gd32/arm/gd32470i-eval/applications/main.c
+++ bsp/gd32/arm/gd32470i-eval/applications/main.c
@@ -1,33 +1,82 @@
 /*
- * Copyright (c) 2006-2025, RT-Thread Development Team
+ * Copyright (c) 2006-2026, RT-Thread Development Team
  *
  * SPDX-License-Identifier: Apache-2.0
  *
  * Change Logs:
  * Date           Author       Notes
- * 2021-08-20     BruceOu      first implementation
- * 2025-08-20     WangShun     change the LED pins
+ * 2026-07-28     Zhiwei Zhang first implementation for GD32470I-EVAL
  */
 
-#include <stdio.h>
 #include <rtthread.h>
 #include <rtdevice.h>
 #include <board.h>
+#include <string.h>
+
+#define GD32_I2C_EEPROM_TEST
+#define GD32_SPI_TEST
 
 /* defined the LED1 pin: PF10 */
-#define LED1_PIN GET_PIN(F, 10)
+#define LED1_PIN    GET_PIN(F, 10)   /* LED1 on PF10 */
+
+#ifdef GD32_I2C_EEPROM_TEST
+#include "at24cxx.h"
+#define BUFFER_SIZE    256
+#define I2C_SERIAL     "hwi2c0"
+rt_uint8_t buf[16];
+rt_uint8_t i2c_buffer_write[BUFFER_SIZE];
+rt_uint8_t i2c_buffer_read[BUFFER_SIZE];
+uint8_t i2c_24c02_test(void);
+#endif
+
+#ifdef GD32_SPI_TEST
+#define BUS_NAME     "spi5"
+#define SPI_NAME     "spi05"
+/* SPI5 CS pin: PI8 */
+#define SPI_CS_PIN   GET_PIN(I, 8)
+
+uint8_t send_id = 0x9F;
+uint8_t WREN = 0x06;
+uint8_t WRITE = 0x02;
+uint8_t READ = 0x03;
+uint8_t SE = 0x20;
+uint8_t recei_id[4] = {0};
+uint8_t  tx_buffer[200];
+uint8_t  rx_buffer[200];
+
+static void spi_sample(void);
+#endif
 
 int main(void)
 {
     int count = 1;
 
-    /* set LED1 pin mode to output */
+    /* set LED pin mode to output */
     rt_pin_mode(LED1_PIN, PIN_MODE_OUTPUT);
+
+    rt_kprintf("Hello GD32470I-EVAL!\n");
+    rt_kprintf("RT-Thread BSP adaptation successful!\n");
+    rt_kprintf("System Clock: %d Hz\n", SystemCoreClock);
+
+#ifdef GD32_I2C_EEPROM_TEST
+    struct rt_i2c_bus_device *i2c0_dev;
+    i2c0_dev = rt_i2c_bus_device_find(I2C_SERIAL);
+    if(i2c_24c02_test() != 0){
+        rt_kprintf("I2C-AT24C02 test passed!\n\r");
+    }
+#endif
+
+#ifdef GD32_SPI_TEST
+    spi_sample();
+#endif
 
     while (count++)
     {
+        /* turn on LED1 */
         rt_pin_write(LED1_PIN, PIN_HIGH);
         rt_thread_mdelay(500);
+
+        /* turn off LED1 */
         rt_pin_write(LED1_PIN, PIN_LOW);
         rt_thread_mdelay(500);
     }
@@ -35,3 +84,109 @@ int main(void)
     return RT_EOK;
 }
 
+
+#ifdef GD32_SPI_TEST
+
+static void spi_sample(void)
+{
+    uint8_t address[4] = {0x20,00,00,04};
+    uint8_t waddress[4] = {0x02,00,00,04};
+    uint8_t raddress[4] = {0x03,00,00,04};
+
+    static struct rt_spi_device *spi_dev = RT_NULL;
+    struct rt_spi_configuration cfg;
+
+    for(int i = 0; i < 200; i++){
+        tx_buffer[i] = i;
+    }
+
+    spi_dev = (struct rt_spi_device *)rt_malloc(sizeof(struct rt_spi_device));
+    rt_hw_spi_device_attach(BUS_NAME, SPI_NAME, SPI_CS_PIN);
+
+    cfg.data_width = 8;
+    cfg.mode   = RT_SPI_MASTER | RT_SPI_MODE_0 | RT_SPI_MSB;
+    cfg.max_hz =  2 *1000 *1000;
+
+    spi_dev = (struct rt_spi_device *)rt_device_find(SPI_NAME);
+    spi_dev->bus->owner = spi_dev;
+    if (RT_NULL == spi_dev)
+    {
+        rt_kprintf("spi sample run failed! can't find %s device!\n", SPI_NAME);
+    }
+    rt_spi_configure(spi_dev, &cfg);
+
+    /* READ FLASH ID */
+    rt_spi_send_then_recv((struct rt_spi_device *)spi_dev, (uint8_t *)&send_id, 1,(uint8_t *)recei_id, 3);
+    rt_kprintf("use rt_spi_transfer_message() read gd25q ID is:%x,%x,%x\n", recei_id[0], recei_id[1],recei_id[2]);
+
+    /* WRITE ENABLE */
+    rt_spi_transfer((struct rt_spi_device *)spi_dev, (uint8_t *)&WREN,(uint8_t *)recei_id, 1);
+
+    /* ERASE SECTOR */
+    rt_spi_transfer((struct rt_spi_device *)spi_dev, (uint8_t *)address,(uint8_t *)recei_id, 4);
+    rt_thread_mdelay(100);
+
+    /* WRITE ENABLE */
+    rt_spi_transfer((struct rt_spi_device *)spi_dev, (uint8_t *)&WREN,(uint8_t *)recei_id, 1);
+
+    /* WRITE TO PAGE */
+    rt_spi_send_then_send((struct rt_spi_device *)spi_dev, (uint8_t *)waddress, 4,(uint8_t *)tx_buffer, 200);
+    rt_thread_mdelay(50);
+
+    /* READ TO BUFFER */
+    rt_spi_send_then_recv((struct rt_spi_device *)spi_dev, (uint8_t *)raddress, 4,(uint8_t *)rx_buffer, 200);
+    rt_thread_mdelay(20);
+
+    if(0 == memcmp(rx_buffer, tx_buffer, 200)) {
+        rt_kprintf("spi flash write and read test success.\r\n");
+    } else {
+        rt_kprintf("spi flash write and read test failed.\r\n");
+    }
+}
+
+MSH_CMD_EXPORT(spi_sample, dspi_sample);
+#endif
+
+#ifdef GD32_I2C_EEPROM_TEST
+
+uint8_t i2c_24c02_test(void)
+{
+    at24cxx_device_t ati2c;
+    ati2c = at24cxx_init(I2C_SERIAL, 0x00);
+
+    if (ati2c != NULL)
+    {
+       rt_kprintf("\r\n Found eeprom \r\n");
+    }
+    uint16_t i;
+    uint8_t i2c_buffer_write[BUFFER_SIZE];
+    uint8_t i2c_buffer_read[BUFFER_SIZE];
+
+    rt_kprintf("\r\n I2C-AT24C02 writing...\r\n");
+
+    /* initialize i2c_buffer_write */
+    for(i = 0; i < BUFFER_SIZE; i++) {
+        i2c_buffer_write[i] = i+8;
+    }
+    /* EEPROM data write */
+    if(at24cxx_page_write(ati2c,0x00,i2c_buffer_write, BUFFER_SIZE) == RT_EOK) {
+           rt_kprintf("I2C-AT24C02 Finish writing...\r\n");
+    }
+
+    rt_kprintf("I2C-AT24C02 reading...\r\n");
+    /* EEPROM data read */
+    if(at24cxx_page_read(ati2c,0x00,i2c_buffer_read, BUFFER_SIZE) == RT_EOK) {
+           rt_kprintf("I2C-AT24C02 Finish reading...\r\n");
+    }
+
+    /* compare the read buffer and write buffer */
+    for(i = 0; i < BUFFER_SIZE; i++) {
+        if(i2c_buffer_read[i] != i2c_buffer_write[i]) {
+            rt_kprintf("0x%02X ", i2c_buffer_read[i]);
+            rt_kprintf("\r\n rr:data read and write aren't matching.\n\r");
+            return 0;
+        }
+    }
+    return 1;
+}
+#endif
